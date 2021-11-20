a41e51a3bbbd9076e434cf754ba05e42ed5ef38a
chanfuku <koheiebato@gmail.com>
2021-11-20 09:35:28 +0900

    Noteクラスにソフトデリートを追加

--- web/laravel/app/Models/Note.php
+++ web/laravel/app/Models/Note.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Note extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     // ブラックリスト。ここに記載されたカラムは書き込み禁止。この定義を削除すると登録時にエラー発生するので注意。
     protected $guarded = [];
